0c4eb7a0c14b1e33bbd4063748485726ec5e03de
NikhilPatnaik1834 <nikhil.patnaik@hotmail.co.uk>
2021-11-10 18:39:16 +0000

    Redux configuration

---
+++ frontend/src/Controllers/bugController.js
@@ -0,0 +1,32 @@
+import bugModel from '../Models/bugModel';
+
+export function retieveBugs(){
+    let data = [];
+
+    data.push(new bugModel({
+        _id:23456789,
+        name:"Crash on load",
+        details:"Crashes after 3 seconds",
+        steps:"Open application and it will crash",
+        version:"V2.0",
+        assigned:"Nikhil Patnaik",
+        creator:"John Doe",
+        priority:1,
+        time:"17:55"
+    }))
+    data.push(new bugModel({
+        _id:23456789,
+        name:"Wont load",
+        details:"Crashes after 3 seconds",
+        steps:"Open application and it will crash",
+        version:"V2.0",
+        assigned:"Nik",
+        creator:"John Doe",
+        priority:3,
+        time:"17:55"
+    }))
+
+    let sorted = data.sort((a,b)=>{return a.priority - b.priority});
+    return sorted;
+
+}
